8839901843d8cbcefbdf4fe8b0d9cc7271f6690d
Ashutosh Yadav <ashuyadav1412@gmail.com>
2023-12-30 21:03:44 +0530

    Pushing working local copy of Eureka Server.

---
+++ src/test/java/com/sugardaddy/eurekaServer/FinanceTrackingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugardaddy.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
